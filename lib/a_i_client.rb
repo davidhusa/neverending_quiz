dccbe53fd5278bbaa846fbdb4a1ebc902e6d0661
David Husa <davidhusa@gmail.com>
2024-08-07 17:53:21 -0700

    refactor question generator, use system prompt

--- lib/a_i_client.rb
+++ lib/a_i_client.rb
@@ -16,18 +16,21 @@ class AIClient
     )
   end
 
-  def send_system_message(message)
-    send_message(message, role: 'system')
-  end
-
-  def send_message(message, role: 'user')
+  def send_message(message, system_prompt: nil)
     response = @client.chat(parameters: {
                               model: DEFAULT_MODEL,
-                              messages: [{ role:, content: message }],
+                              messages: messages_array(message, system_prompt),
                               temperature: 0.7
                             })
     @last_response = response
 
     response.dig('choices', 0, 'message', 'content')
+  end
+
+  def messages_array(message, system_prompt)
+    array = []
+    array << { role: 'system', content: system_prompt } if system_prompt.present?
+    array << { role: 'user', content: message }
+    array
   end
 end
--- lib/question_generator/generator.rb
+++ lib/question_generator/generator.rb
@@ -5,7 +5,7 @@ module QuestionGenerator
   class Generator
     attr_reader :ai_client
 
-    def initialize(ai_client: AIClient.new, number_of_questions: 50)
+    def initialize(ai_client: AIClient.new, number_of_questions: 3)
       @ai_client = ai_client
       @number_of_questions = number_of_questions
     end
@@ -37,8 +37,7 @@ module QuestionGenerator
     end
 
     def retrieve_raw_question_text(topic)
-      # send_system_prompt_if_not_sent # not sure if system prompt is necessary or useful for this
-      @ai_client.send_message(trivia_prompt(topic))
+      @ai_client.send_message(trivia_prompt(topic), system_prompt:)
     end
 
     def process_response_text_to_hashes(question_response_text)
@@ -74,22 +73,18 @@ module QuestionGenerator
 
     def trivia_prompt(topic)
       <<~PROMPT.chomp
-        Generate to #{@number_of_questions} multiple choice questions about #{topic}.
-        Include four answers with only one correct answer that is always the first option.
-        Do not include any text aside from the questions and answers.
-        Separate each question with "=====".
-        Keep the questions and answers each on their own, singular line.
+        Generate to #{@number_of_questions} questions about #{topic}.
       PROMPT
     end
 
-    # def system_prompt
-    # end
-
-    # def send_system_prompt_if_not_sent
-    #   return if @prompt_sent
-
-    #   @ai_client.send_system_message(system_prompt)
-    #   @prompt_sent = true
-    # end
+    def system_prompt
+      <<~PROMPT.chomp
+        Generate multiple choice questions with four answers with only one correct answer that is always the first option.
+        Do not include any text aside from the questions and answers.
+        Separate each question with "=====".
+        Keep the questions and answers each on their own, singular line.
+        Return only questions with four possible answers, return nothing if you cannot generate a question with four possible answers.
+      PROMPT
+    end
   end
 end
